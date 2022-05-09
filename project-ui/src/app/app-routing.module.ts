56854e800238a470d2feba3450bc83cfeab686a8
CosminArtene <cosmin.artene@thunder-agency.com>
2022-05-09 18:26:13 +0300

    added tabs and header component

--- project-ui/src/app/app-routing.module.ts
+++ project-ui/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+}, {
+  path: 'knowledge',
+  //TODO add each component for it's specific route, also don't forget to
+  // add the generic header component to each one of them
+  component: HomeComponent
+}, {
+  path: 'quizzes',
+  component: HomeComponent
+}, {
+  path: 'flashcards',
+  component: HomeComponent
+}, {
+  path: 'notes',
+  component: HomeComponent
+}, {
+  path: 'leaderboards',
+  component: HomeComponent
+}, {
+  path: 'friends',
+  component: HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
